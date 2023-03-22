6992a60e40b4eb38dfb6c0a7c5a63a15a3ab35e5
FannyMayorga1999 <fannymayorga1999@gmail.com>
2023-03-21 21:40:06 -0500

    props

--- app-example-02/src/components/ExpenseItem.js
+++ app-example-02/src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <div>March 28th 2023</div>
+      <div>{props.date.toISOString()}</div>
       <div>
         <h2 className="expense-item__description">
-          Car Insurance
+        {props.title}
         </h2>
-        <div className="expense-item__price">$294.67</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
